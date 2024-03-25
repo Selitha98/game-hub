b62bb3ff3ddd1f3577450aea785c1b086d1bcd0b
Selitha98 <selithasasmitha@gmail.com>
2024-03-25 16:07:20 +0530

    Adding Nav bar structure and dark and light color theme for web site

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.webp"
 const NavBar = () => {
   return (
     <HStack>
-        <Image src={logo} boxSize={10}/>
+        <Image src={logo} boxSize={10 }/>
     </HStack>
   )
 }
